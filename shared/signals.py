b231cf13cec9566c7cab834caab1caec347a3738
Tungston-labs <support@tungstonlabs.com>
2025-08-07 16:45:50 +0530

    lco bulk upload

--- shared/signals.py
+++ shared/signals.py
@@ -3,9 +3,9 @@ from django.dispatch import receiver
 from django.contrib.contenttypes.models import ContentType
 from shared.models import ActivityLog
 from django.utils.timezone import now
+from django.db import ProgrammingError, OperationalError, DatabaseError
 
 def get_user_from_instance(instance):
-    # Try to get `created_by` or `updated_by`
     return getattr(instance, 'updated_by', None) or getattr(instance, 'created_by', None)
 
 @receiver(post_save)
@@ -16,13 +16,17 @@ def log_create_or_update(sender, instance, created, **kwargs):
     user = get_user_from_instance(instance)
     action = "created" if created else "updated"
 
-    ActivityLog.objects.create(
-        user=user,
-        action=action,
-        model_name=sender.__name__,
-        object_id=str(instance.pk),
-        description=f"{user} {action} {sender.__name__} ({instance})",
-    )
+    try:
+        ActivityLog.objects.create(
+            user=user,
+            action=action,
+            model_name=sender.__name__,
+            object_id=str(instance.pk),
+            description=f"{user} {action} {sender.__name__} ({instance})",
+        )
+    except (ProgrammingError, OperationalError, DatabaseError):
+        # Likely running during migration; ignore if ActivityLog table doesn't exist yet
+        pass
 
 @receiver(post_delete)
 def log_delete(sender, instance, **kwargs):
@@ -31,10 +35,13 @@ def log_delete(sender, instance, **kwargs):
 
     user = get_user_from_instance(instance)
 
-    ActivityLog.objects.create(
-        user=user,
-        action="deleted",
-        model_name=sender.__name__,
-        object_id=str(instance.pk),
-        description=f"{user} deleted {sender.__name__} ({instance})",
-    )
+    try:
+        ActivityLog.objects.create(
+            user=user,
+            action="deleted",
+            model_name=sender.__name__,
+            object_id=str(instance.pk),
+            description=f"{user} deleted {sender.__name__} ({instance})",
+        )
+    except (ProgrammingError, OperationalError, DatabaseError):
+        pass
