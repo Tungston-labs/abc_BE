a77228931048822b93fa19fe539b117d0689c1d4
Tungston-labs <support@tungstonlabs.com>
2025-10-27 05:17:41 +0000

    pagination

--- shared/paginations.py
+++ shared/paginations.py
@@ -6,13 +6,14 @@ import math
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 15  # Default items per page
     page_size_query_param = 'page_size'
-    max_page_size = 100
+    max_page_size =5000
 
     def get_paginated_response(self, data):
         total_pages = math.ceil(self.page.paginator.count / self.get_page_size(self.request))
         return Response({
             'count': self.page.paginator.count,
             'total_pages': total_pages,
+
             'current_page': self.page.number,
             'next': self.get_next_link(),
             'previous': self.get_previous_link(),
