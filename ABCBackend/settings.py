cd5562183142d8fe0591d89549917fb3580a06a9
Tungston-labs <support@tungstonlabs.com>
2025-11-10 15:18:29 +0530

    persist login

--- ABCBackend/settings.py
+++ ABCBackend/settings.py
@@ -165,8 +165,8 @@ from datetime import timedelta
 
 SIMPLE_JWT = {
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=6),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
 }
