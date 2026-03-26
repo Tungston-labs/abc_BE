092607d41b5274a9da1989d82d735f74c452ff4b
Tungston-labs <support@tungstonlabs.com>
2026-03-26 12:55:14 +0530

    added lco name

--- tickets/serializers.py
+++ tickets/serializers.py
@@ -7,13 +7,22 @@ class TicketAttachmentSerializer(serializers.ModelSerializer):
         model = TicketAttachment
         fields = ["id", "file", "uploaded_at"]
 
+
 class TicketSerializer(serializers.ModelSerializer):
     attachments = TicketAttachmentSerializer(many=True, read_only=True)
     lco_name = serializers.CharField(source="lco.name", read_only=True)
 
-
     class Meta:
         model = Ticket
-        fields = "__all__"
-        read_only_fields = ['created_by', 'source', 'created_at']
-
+        fields = [
+            "id",
+            "name",
+            "notes",
+            "status",
+            "priority",
+            "category",
+            "created_at",
+            "lco",
+            "lco_name",  
+            "attachments",
+        ]
