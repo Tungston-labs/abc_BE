eb359188daa666864d28feafa3cf8d673b2099d5
Tungston-labs <support@tungstonlabs.com>
2026-06-29 14:47:55 +0530

    expiry changes

--- customers/utils.py
+++ customers/utils.py
@@ -1,6 +1,11 @@
-from datetime import date
+from datetime import date, timedelta
 from .models import Customer
 
+
 def get_today_expiring_customers():
-    today = date.today()
-    return Customer.objects.filter(expiry_date=today).select_related("lco")
+    from_date = date.today()
+    to_date = from_date + timedelta(days=5)
+
+    return Customer.objects.filter(
+        expiry_date__range=(from_date, to_date)
+    ).select_related("lco")
