516dcf8e8be82fa84db5303aa7ec2550008731b4
Tungston-labs <support@tungstonlabs.com>
2026-07-23 17:03:16 +0530

    xtra net integration

--- network/services/isp_handler.py
+++ network/services/isp_handler.py
@@ -3,7 +3,7 @@ from network.services.extranet import fetch_extranet_data
 
 
 def fetch_isp_data(mapping):
-    isp_name = mapping.isp.name.lower()
+    isp_name = mapping.isp.name.strip().lower()
 
     print(f"📡 Provider : {mapping.isp.name}")
     print(f"👤 Partner  : {mapping.partner_name}")
@@ -12,8 +12,8 @@ def fetch_isp_data(mapping):
         print("➡ Calling Stampede API")
         return fetch_stampede_data(mapping)
 
-    elif isp_name == "extranet":
-        print("➡ Calling Extranet API")
+    elif isp_name in ["xtra net", "extranet"]:
+        print("➡ Calling XTRA NET API")
         return fetch_extranet_data(mapping)
 
     raise Exception(f"Unsupported ISP: {isp_name}")
